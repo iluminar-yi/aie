12717db95606e9486a61cd9807a231820a222c9e
Iluminar <yi.iluminar@gmail.com>
2020-04-20 12:16:52 -0700

    Add react and react-dom to WebPack externals to prevent loading multiple instances

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,16 @@
+'use strict';
+
 const isCi = process.env.CI === 'true' || process.env.CI === 'True';
 
 module.exports = {
   root: true,
   extends: [
     'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:react/recommended',
-    // Currently not working, need to define manually
-    // 'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
-    'plugin:jest/all',
   ],
-  plugins: ['react-hooks'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    jsx: true,
-    tsconfigRootDir: '.',
-  },
   env: {
-    browser: true,
     node: true,
-    es2017: true,
-    jest: true,
-  },
-  globals: {
-    __static: true,
+    es2020: true,
   },
   rules: {
     'no-console': isCi ? 'error' : 'warn',
@@ -44,24 +25,6 @@ module.exports = {
         memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
       },
     ],
-    yoda: ['error', 'never', { exceptRange: true }],
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: false,
-        },
-      },
-    ],
-    'react/prop-types': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
+    strict: ['error', 'safe'],
   },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = {
   bracketSpacing: true,
   jsxBracketSameLine: true,
